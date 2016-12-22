4d2ff8b02424345959f5ff20941d10a8d16bad92
sho arata <sho arata>
2016-12-22 16:29:27 -0500

    added  'save Item' in both edit item mode and new item mode

--- app/src/main/java/com/example/android/inventorywizard/DetailsActivity.java
+++ app/src/main/java/com/example/android/inventorywizard/DetailsActivity.java
@@ -2,7 +2,6 @@ package com.example.android.inventorywizard;
 
 import android.app.LoaderManager;
 import android.content.ActivityNotFoundException;
-import android.content.ClipData;
 import android.content.ContentValues;
 import android.content.CursorLoader;
 import android.content.DialogInterface;
@@ -13,11 +12,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
@@ -37,8 +40,6 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
     private final static int PICKIMAGE = 1;
     /** uri for the current item**/
     Uri mCurrentItemUri;
-    /** if any changes were made in the item form this will be true */
-    private boolean mItemHasChanged = false;
     /**loader id**/
     private final static int LOADER_ID = 2;
     /**Edit text field for the name**/
@@ -53,6 +54,8 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
     EditText mSupplierName;
     /** image view for the producto photo **/
     ImageView mImage;
+    /** hold the selected image URI **/
+    Uri mNewImageUri;
     /** imagepicker button**/
     Button mImagePicker;
     /**sell button**/
@@ -63,8 +66,21 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
     Button mOrderMore;
     /** Contents of the current item **/
     ContentValues mValues;
+    /** tells whether a field of the item has changed **/
+    private boolean mItemHasChanged = false;
+    /** change listener to be attached to the item's field **/
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            mItemHasChanged = true;
+            return false;
+        }
+    };
     /** LOG TAG**/
     private final static String LOG_TAG = DetailsActivity.class.getSimpleName();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,6 +93,12 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
         mEmail= (EditText) findViewById(R.id.edit_text_supplier_email);
         mSupplierName = (EditText) findViewById(R.id.edit_text_supplier_name);
         mImage = (ImageView)findViewById(R.id.detail_image_view);
+        //set up change listener for the inputs
+        mName.setOnTouchListener(mTouchListener);
+        mPrice.setOnTouchListener(mTouchListener);
+        mQuantity.setOnTouchListener(mTouchListener);
+        mEmail.setOnTouchListener(mTouchListener);
+        mSupplierName.setOnTouchListener(mTouchListener);
         //preventing the user from changing the quantity manually
         mQuantity.setFocusable(false);
         //find buttons
@@ -130,6 +152,118 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
         });
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        if(mCurrentItemUri == null){
+            MenuItem delete = menu.findItem(R.id.action_delete);
+            // we're adding new item thus we hide the delete option
+            delete.setVisible(false);
+        }
+        return true;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options
+        getMenuInflater().inflate(R.menu.details_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to save Item
+            case R.id.action_save:
+                saveItem();
+                finish();
+                return true;
+            // Respond to delete Item
+            case R.id.action_delete:
+                showDeleteConfirmationDialog();
+                finish();
+                return true;
+            //if up back button is pressed
+            case android.R.id.home:
+                //if item hasn't changed then go to parent normally
+                if (!mItemHasChanged) {
+                    NavUtils.navigateUpFromSameTask(DetailsActivity.this);
+                    return true;
+                }
+
+                //otherwise show dialog warning the user that the changes made won't be saved
+                DialogInterface.OnClickListener discardButtonClickListener =
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // User clicked "Discard" button, navigate to parent activity.
+                                NavUtils.navigateUpFromSameTask(DetailsActivity.this);
+                            }
+                        };
+
+                // show dialog
+                showUnsavedChangesDialog(discardButtonClickListener);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    /** save new Item **/
+    //qty default to 0
+    private void saveItem(){
+        // if nothing was inserted then do nothing and go back
+        if(allFieldsEmpty()){
+            finish();
+            return;
+        }
+        // creat contentvalue var
+        String name           = mName.getText().toString();
+        int price             = Integer.parseInt(mPrice.getText().toString());
+        int quantity          = 0;
+        String suppliersName  = mSupplierName.getText().toString();
+        String suppliersEmail = mEmail.getText().toString();
+        String imageUri       = mNewImageUri.toString();
+        ContentValues values  = new ContentValues();
+        values.put(ItemEntry.COLUMN_NAME,name);
+        values.put(ItemEntry.COLUMN_PRICE,price);
+        values.put(ItemEntry.COLUMN_SUPPLIER_NAME,suppliersName);
+        values.put(ItemEntry.COLUMN_SUPPLIER_EMAIL,suppliersEmail);
+        values.put(ItemEntry.COLUMN_IMAGE_URI,imageUri);
+        //if current item uri null -> new Item
+        if(mCurrentItemUri == null){
+            values.put(ItemEntry.COLUMN_QUANTITY,quantity);
+            Uri newItemUri  = getContentResolver().insert(ItemEntry.CONTENT_URI,values);
+            if(newItemUri == null){
+                Toast.makeText(DetailsActivity.this, getString(R.string.error_inserting_new_item),Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(DetailsActivity.this, getString(R.string.item_saved),Toast.LENGTH_SHORT).show();
+            }
+        }
+        //otherwise -> editing an existing item
+        else{
+            values.put(ItemEntry.COLUMN_QUANTITY,quantity);
+            int numItemsUpdated= getContentResolver().update(mCurrentItemUri,values,null,null);
+            if(numItemsUpdated > 0){
+                (Toast.makeText(this,getString(R.string.item_succesfuly_edited),Toast.LENGTH_SHORT)).show();
+            }
+            else{
+                (Toast.makeText(this,R.string.error_editing_item,Toast.LENGTH_SHORT)).show();
+            }
+        }
+
+    }
+    /** tells if all the fields are empty **/
+    private boolean allFieldsEmpty(){
+        return mName.getText().toString().isEmpty() && mPrice.getText().toString().isEmpty() && mQuantity.getText().toString().isEmpty()&& mEmail.getText().toString().isEmpty()&& mSupplierName.getText().toString().isEmpty();
+    }
+    /** shows confirmation dialog before deleting **/
+    private void showDeleteConfirmationDialog(){
+        //!!!
+    }
+    /** shows dialog that changes won't be saved **/
+    private void showUnsavedChangesDialog(DialogInterface.OnClickListener discardListener){
+        // !!!
+        finish();
+    }
     /** call intent to email the provider for more items **/
     private void placeOrder(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -279,17 +413,24 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
         intent.setType("image/*");
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICKIMAGE);
     }
-    /** when the user is done selecting an image, this will store the image URI in the data base for later usage **/
+    /** when the user is done selecting an image, this will store the image URI in member variable for later usage **/
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
         switch(requestCode) {
             case PICKIMAGE:
                 if(resultCode == RESULT_OK){
-                    Uri selectedImage = imageReturnedIntent.getData();
-                    ContentValues value = new ContentValues();
-                    value.put(ItemEntry.COLUMN_IMAGE_URI,selectedImage.toString());
-                    getContentResolver().update(mCurrentItemUri,value,null,null);
-                    mImage.setImageBitmap(getBitmapFromUri(selectedImage));
+                    mNewImageUri = imageReturnedIntent.getData();
+                    mImage.setVisibility(View.VISIBLE);
+                    // the get bitmapfromuri method requires that the layout of the detailsactivity is already drawn specifically the image view
+                    // ongloballayout listener will take care of calling getbitmapfrom uri when the layout is drawn
+                    ViewTreeObserver viewTreeObserver =  mImage.getViewTreeObserver();
+                    viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                        @Override
+                        public void onGlobalLayout() {
+                            mImage.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                            mImage.setImageBitmap(getBitmapFromUri(mNewImageUri));
+                        }
+                    });
                 }
                 break;
         }
@@ -368,13 +509,13 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
             mQuantity.setText(cursor.getString(cursor.getColumnIndex(ItemEntry.COLUMN_QUANTITY)));
             mEmail.setText(cursor.getString(cursor.getColumnIndex(ItemEntry.COLUMN_SUPPLIER_EMAIL)));
             mSupplierName.setText(cursor.getString(cursor.getColumnIndex(ItemEntry.COLUMN_SUPPLIER_NAME)));
+            mNewImageUri  = Uri.parse(cursor.getString(cursor.getColumnIndex(ItemEntry.COLUMN_IMAGE_URI)));
             //copy values to mValues for later usage
             mValues.put(ItemEntry.COLUMN_QUANTITY,cursor.getString(cursor.getColumnIndex(ItemEntry.COLUMN_QUANTITY)));
             mValues.put(ItemEntry.COLUMN_SUPPLIER_NAME,cursor.getString(cursor.getColumnIndex(ItemEntry.COLUMN_SUPPLIER_NAME)));
             mValues.put(ItemEntry.COLUMN_SUPPLIER_EMAIL,cursor.getString(cursor.getColumnIndex(ItemEntry.COLUMN_SUPPLIER_EMAIL)));
 
             if (!cursor.getString(cursor.getColumnIndex(ItemEntry.COLUMN_IMAGE_URI)).isEmpty()) {
-                final Uri imageUri = Uri.parse(cursor.getString(cursor.getColumnIndex(ItemEntry.COLUMN_IMAGE_URI)));
                 mImage.setVisibility(View.VISIBLE);
                 // the get bitmapfromuri method requires that the layout of the detailsactivity is already drawn specifically the image view
                 // ongloballayout listener will take care of calling getbitmapfrom uri when the layout is drawn
@@ -383,7 +524,7 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
                     @Override
                     public void onGlobalLayout() {
                         mImage.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                        mImage.setImageBitmap(getBitmapFromUri(imageUri));
+                        mImage.setImageBitmap(getBitmapFromUri(mNewImageUri));
                     }
                 });
             }
